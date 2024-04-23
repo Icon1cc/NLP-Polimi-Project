3d9f5f34e832bef012fa334ce3d15f8ee42f5090
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-04-23 23:15:09 +0200

    added dataset

--- Rishabh/dataset/data.py
+++ Rishabh/dataset/data.py
@@ -1,6 +1,13 @@
 from datasets import load_dataset
 
-# Load the dataset with timeout
-dataset = load_dataset("medalpaca/medical_meadow_medical_flashcards", timeout=200)
+try:
+    # Load the dataset
+    dataset = load_dataset("medalpaca/medical_meadow_medical_flashcards")
+    print(dataset)
 
-print(dataset)
+    # Save the dataset to disk
+    save_path = '/Users/icon1c/Documents/Semester 2/Natural Language Processing/NLP-Polimi-Project/Rishabh/dataset'
+    dataset.save_to_disk(save_path)
+    print(f"Dataset saved successfully to {save_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
