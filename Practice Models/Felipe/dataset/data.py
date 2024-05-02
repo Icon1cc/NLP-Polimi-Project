1f8f4202f204d6096e582ddc68a8dddecfa4ad36
febagni <febagni@usp.br>
2024-05-02 10:49:01 +0200

    :sparkles: Add notebook for training fine-tune GPT model

---
+++ Practice Models/Felipe/dataset/data.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+
+try:
+    # Load the dataset
+    dataset = load_dataset("medalpaca/medical_meadow_medical_flashcards")
+    print(dataset)
+
+    # Save the dataset to disk
+    save_path = '/Users/icon1c/Documents/Semester 2/Natural Language Processing/NLP-Polimi-Project/Practice Models/Rishabh/dataset'
+    dataset.save_to_disk(save_path)
+    print(f"Dataset saved successfully to {save_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
